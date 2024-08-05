e3c8e2815d26ebd9634fcd573eccda76240d36f1
JeroCaller <j69m17@gmail.com>
2024-08-05 18:01:47 +0900

    오타 및 글 수정

--- resources/2024-08-04/site_users.sql
+++ resources/2024-08-04/site_users.sql
@@ -4,25 +4,25 @@ create table site_users (
 	class_number INT,
 	mileage INT,
 	username VARCHAR(50),
-	aver_purchage INT
+	aver_purchase INT
 );
-insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchage) values (1, '2023-10-23', 1, 1549, 'pcopozio0', 616);
-insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchage) values (2, '2024-01-09', 4, 2712, 'eszanto1', null);
-insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchage) values (3, '2023-08-15', 5, 1129, 'jmatuszkiewicz2', 855);
-insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchage) values (4, '2024-06-04', 2, 4238, 'egudgion3', null);
-insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchage) values (5, '2023-12-06', 3, 4632, 'gfittes4', 592);
-insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchage) values (6, '2023-09-23', 4, 2235, 'dbuttrum5', null);
-insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchage) values (7, '2024-07-21', 5, 1891, 'lfree6', 547);
-insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchage) values (8, '2023-07-03', 3, 1396, 'bleggis7', null);
-insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchage) values (9, '2024-05-04', 2, 4058, 'kmardle8', null);
-insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchage) values (10, '2023-05-06', 5, 4042, 'dsummerside9', 249);
-insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchage) values (11, '2023-01-08', 2, 1282, 'fwillimonta', null);
-insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchage) values (12, '2024-02-21', 1, 1546, 'atailourb', null);
-insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchage) values (13, '2024-07-09', 2, 3492, 'dmulgrewc', 807);
-insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchage) values (14, '2024-06-14', 4, 4908, 'ksimeolid', null);
-insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchage) values (15, '2024-05-16', 1, 4640, 'lscaddinge', 656);
-insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchage) values (16, '2023-10-10', 5, 3486, 'srenonf', 322);
-insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchage) values (17, '2024-03-27', 4, 2954, 'aledesg', 850);
-insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchage) values (18, '2023-01-06', 1, 2632, 'hshickleh', null);
-insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchage) values (19, '2022-09-11', 5, 3712, 'lglandersi', null);
-insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchage) values (20, '2024-03-13', 1, 2596, 'dionsj', 761);
+insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchase) values (1, '2023-10-23', 1, 1549, 'pcopozio0', 616);
+insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchase) values (2, '2024-01-09', 4, 2712, 'eszanto1', null);
+insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchase) values (3, '2023-08-15', 5, 1129, 'jmatuszkiewicz2', 855);
+insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchase) values (4, '2024-06-04', 2, 4238, 'egudgion3', null);
+insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchase) values (5, '2023-12-06', 3, 4632, 'gfittes4', 592);
+insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchase) values (6, '2023-09-23', 4, 2235, 'dbuttrum5', null);
+insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchase) values (7, '2024-07-21', 5, 1891, 'lfree6', 547);
+insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchase) values (8, '2023-07-03', 3, 1396, 'bleggis7', null);
+insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchase) values (9, '2024-05-04', 2, 4058, 'kmardle8', null);
+insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchase) values (10, '2023-05-06', 5, 4042, 'dsummerside9', 249);
+insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchase) values (11, '2023-01-08', 2, 1282, 'fwillimonta', null);
+insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchase) values (12, '2024-02-21', 1, 1546, 'atailourb', null);
+insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchase) values (13, '2024-07-09', 2, 3492, 'dmulgrewc', 807);
+insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchase) values (14, '2024-06-14', 4, 4908, 'ksimeolid', null);
+insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchase) values (15, '2024-05-16', 1, 4640, 'lscaddinge', 656);
+insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchase) values (16, '2023-10-10', 5, 3486, 'srenonf', 322);
+insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchase) values (17, '2024-03-27', 4, 2954, 'aledesg', 850);
+insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchase) values (18, '2023-01-06', 1, 2632, 'hshickleh', null);
+insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchase) values (19, '2022-09-11', 5, 3712, 'lglandersi', null);
+insert into site_users (member_id, sign_up_date, class_number, mileage, username, aver_purchase) values (20, '2024-03-13', 1, 2596, 'dionsj', 761);
